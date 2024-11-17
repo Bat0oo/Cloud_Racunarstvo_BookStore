e3022896454e486c3233f34200c52f8ca165e4e5
Bat0oo <miloradmaksic01@gmail.com>
2024-11-17 20:24:00 +0100

    Bank and bookstore services

--- BookStore/BankService/BankService.cs
+++ BookStore/BankService/BankService.cs
@@ -2,10 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Fabric;
 using System.Linq;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Common;
+using Common.Interfaces;
+using Common.Models;
+using Microsoft.ServiceFabric.Data;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 
 namespace BankService
@@ -13,11 +19,131 @@ namespace BankService
     /// <summary>
     /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
-    internal sealed class BankService : StatefulService
+    internal sealed class BankService : StatefulService, IBank, Common.Interfaces.ITransaction
     {
+        private IReliableDictionary<long, Client>? _clientDictionary;
         public BankService(StatefulServiceContext context)
             : base(context)
         { }
+
+        private async Task InitializeClientDictionaryAsync()
+        {
+            _clientDictionary = await StateManager.GetOrAddAsync<IReliableDictionary<long, Client>>("clientDictionary");
+        }
+
+
+        public async Task<string> EnlistMoneyTransfer(long? userSend, long? userReceive, double? amount)
+        {
+            using (var transaction = StateManager.CreateTransaction())
+            {
+                ConditionalValue<Client> clientToSend = await _clientDictionary!.TryGetValueAsync(transaction, userSend!.Value);
+                ConditionalValue<Client> clientToReceive = await _clientDictionary!.TryGetValueAsync(transaction, userReceive!.Value);
+
+                var transactionContext = new TransactionContext { ClientToSend = clientToSend, ClientToReceive = clientToReceive };
+
+                if (!await Prepare(transactionContext, amount!.Value))
+                {
+                    return null!;
+                }
+
+                var clientToSendUpdate = clientToSend.Value;
+                var clientToReceiveUpdate = clientToReceive.Value;
+
+                clientToSendUpdate.BankAccount -= amount;
+                clientToReceiveUpdate.BankAccount += amount;
+
+                await _clientDictionary.TryUpdateAsync(transaction, userSend!.Value, clientToSendUpdate, clientToSend.Value);
+                await _clientDictionary.TryUpdateAsync(transaction, userReceive!.Value, clientToReceiveUpdate, clientToReceive.Value);
+
+                //await transaction.CommitAsync();
+
+                //return string.Empty;
+
+                return await FinishTransaction(transaction);
+            }
+        }
+        public async Task<string> FinishTransaction(Microsoft.ServiceFabric.Data.ITransaction transaction)
+        {
+            try
+            {
+                await Commit(transaction);
+                return string.Empty;
+            }
+            catch
+            {
+                await RollBack(transaction);
+                return null!;
+            }
+        }
+
+        public async Task<List<string>> ListClients()
+        {
+            await InitializeClientDictionaryAsync();
+
+            var clients = new List<Client>()
+            {
+                new Client() { Id = 1, FirstName = "Milorad", LastName = "Maksic", DateOfBirth = new DateTime(1990, 1, 1), BankName = "OTP Bank", BankAccount = 1000.00, BankMembership = BankMembership.First.GetDescription() },
+                new Client() { Id = 2, FirstName = "Ime", LastName = "Prezime", DateOfBirth = new DateTime(1985, 5, 15), BankName = "UniCredit", BankAccount = 750.50, BankMembership = BankMembership.Second.GetDescription() },
+                new Client() { Id = 3, FirstName = "Mike", LastName = "Tyson", DateOfBirth = new DateTime(1995, 10, 8), BankName = "Banka", BankAccount = 500.75, BankMembership = BankMembership.Third.GetDescription() },
+                new Client() { Id = 4, FirstName = "Jake", LastName = "Paul", DateOfBirth = new DateTime(1982, 3, 20), BankName = "Banka postanske stedionice", BankAccount = 2000.25, BankMembership = BankMembership.Fourth.GetDescription() },
+                new Client() { Id = 5, FirstName = "John", LastName = "Doe", DateOfBirth = new DateTime(1998, 7, 3), BankName = "Jos neka banka", BankAccount = 1200.90, BankMembership = BankMembership.Second.GetDescription() }
+            };
+
+            using (var transaction = StateManager.CreateTransaction())
+            {
+                foreach (Client client in clients)
+                    await _clientDictionary!.AddOrUpdateAsync(transaction, client.Id!.Value, client, (k, v) => v);
+
+                //await transaction.CommitAsync();
+                await FinishTransaction(transaction);
+            }
+
+            var clientsJson = new List<string>();
+
+            using (var transaction = StateManager.CreateTransaction())
+            {
+                var enumerator = (await _clientDictionary!.CreateEnumerableAsync(transaction)).GetAsyncEnumerator();
+
+                while (await enumerator.MoveNextAsync(CancellationToken.None))
+                {
+                    var client = enumerator.Current.Value;
+                    // clientsJson.Add(JsonConvert.SerializeObject(client));
+                    clientsJson.Add(JsonSerializer.Serialize(client));
+                }
+            }
+
+            return clientsJson;
+        }
+
+        public async Task Commit(Microsoft.ServiceFabric.Data.ITransaction transaction)
+        {
+            await transaction.CommitAsync();
+        }
+        public async Task<bool> Prepare(TransactionContext context, object value)
+        {
+            if (!(value is double doubleParameter)) //value/amount
+            {
+                return false;
+            }
+
+            if (!context.ClientToSend.HasValue || !context.ClientToReceive.HasValue)
+            {
+                return false;
+            }
+
+            if (context.ClientToSend.Value.BankAccount < doubleParameter)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task RollBack(Microsoft.ServiceFabric.Data.ITransaction transaction)
+        {
+             transaction.Abort();
+            //await
+        }
 
         /// <summary>
         /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
@@ -28,7 +154,8 @@ namespace BankService
         /// <returns>A collection of listeners.</returns>
         protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
         {
-            return new ServiceReplicaListener[0];
+            //return new ServiceReplicaListener[0];
+            return this.CreateServiceRemotingReplicaListeners();
         }
 
         /// <summary>
--- BookStore/BookStoreService/BookStoreService.cs
+++ BookStore/BookStoreService/BookStoreService.cs
@@ -2,10 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Fabric;
 using System.Linq;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Common;
+using Common.Interfaces;
+using Common.Models;
+using Microsoft.ServiceFabric.Data;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 
 namespace BookStoreService
@@ -13,11 +19,153 @@ namespace BookStoreService
     /// <summary>
     /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
-    internal sealed class BookStoreService : StatefulService
+    internal sealed class BookStoreService : StatefulService, IBookstore, Common.Interfaces.ITransaction
     {
+        private IReliableDictionary<long, Book>? _bookDictionary;
         public BookStoreService(StatefulServiceContext context)
             : base(context)
         { }
+        private async Task InitializeBookDictionaryAsync()
+        {
+            _bookDictionary = await StateManager.GetOrAddAsync<IReliableDictionary<long, Book>>("bookDictionary");
+        }
+
+
+
+        public async Task<string> EnlistPurchase(long? bookId, uint? count)
+        {
+            using (var transaction = StateManager.CreateTransaction())
+            {
+                ConditionalValue<Book> book = await _bookDictionary!.TryGetValueAsync(transaction, bookId!.Value);
+                var transactionContext = new TransactionContext { Book = book };
+
+                if (!await Prepare(transactionContext, count!.Value))
+                {
+                    return null!;
+                }
+
+                var bookToUpdate = book.Value;
+
+                bookToUpdate.Quantity -= Convert.ToInt32(count);
+
+                await _bookDictionary.TryUpdateAsync(transaction, bookId!.Value, bookToUpdate, book.Value);
+
+                //await transaction.CommitAsync();
+
+                //return string.Empty;
+
+                return await FinishTransaction(transaction);
+            }
+        }
+
+        public async Task<string> GetItem(long? bookId)
+        {
+            using (var transaction = StateManager.CreateTransaction())
+            {
+                var book = await _bookDictionary!.TryGetValueAsync(transaction, bookId!.Value);
+
+                return JsonSerializer.Serialize(book.Value);
+            }
+
+            throw null!;
+        }
+
+        public async Task<string> GetItemPrice(long? bookId)
+        {
+            using (var transaction = StateManager.CreateTransaction())
+            {
+                var book = await _bookDictionary!.TryGetValueAsync(transaction, bookId!.Value);
+
+                return book.Value.Price!.Value.ToString();
+            }
+
+            throw null!;
+        }
+
+        public async Task<List<string>> ListAvailableItems()
+        {
+
+            await InitializeBookDictionaryAsync();
+
+            var books = new List<Book>()
+            {
+                new Book(){ Id = 1, Title = "Seobe", Author = "Milos Crnjanski", PagesNumber = 281, PublicationYear = 1960, Price = 9.99, Quantity = 10 },
+                new Book(){ Id = 2, Title = "1984", Author = "Dzordz Osvel", PagesNumber = 328, PublicationYear = 1949, Price = 10.60, Quantity = 10 },
+                new Book(){ Id = 3, Title = "Na Drini Cuprija", Author = "Ivo Andric", PagesNumber = 180, PublicationYear = 1925, Price = 22.35, Quantity = 10 },
+                new Book(){ Id = 4, Title = "Tvrdjava", Author = "Mesa Selimovic", PagesNumber = 310 , PublicationYear = 1937, Price = 50.00, Quantity = 10 },
+                new Book(){ Id = 5, Title = "Seobe 2", Author = "Milos Crnjanski", PagesNumber = 279, PublicationYear = 1813, Price = 14.85, Quantity = 10 }
+            };
+
+            using (var transaction = StateManager.CreateTransaction())
+            {
+                foreach (Book book in books)
+                    await _bookDictionary!.AddOrUpdateAsync(transaction, book.Id!.Value, book, (k, v) => v);
+
+                //await transaction.CommitAsync();
+                await FinishTransaction(transaction);
+            }
+
+            var booksJson = new List<string>();
+
+            using (var transaction = StateManager.CreateTransaction())
+            {
+                var enumerator = (await _bookDictionary!.CreateEnumerableAsync(transaction)).GetAsyncEnumerator();
+
+                while (await enumerator.MoveNextAsync(CancellationToken.None))
+                {
+                    var book = enumerator.Current.Value;
+                    //booksJson.Add(JsonConvert.SerializeObject(book));
+                    booksJson.Add(JsonSerializer.Serialize(book));
+                }
+            }
+
+            return booksJson;
+        }
+
+        public async Task<string> FinishTransaction(Microsoft.ServiceFabric.Data.ITransaction transaction)
+        {
+            try
+            {
+                await Commit(transaction);
+                return string.Empty;
+            }
+            catch
+            {
+                await RollBack(transaction);
+                return null!;
+            }
+        }
+        public async Task<bool> Prepare(TransactionContext context, object value)
+        {
+
+            if (!(value is uint uintParameter))
+            {
+                return false;
+            }
+
+            if (!context.Book.HasValue)
+            {
+                return false;
+            }
+
+            if (context.Book.Value.Quantity < uintParameter)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task Commit(Microsoft.ServiceFabric.Data.ITransaction transaction)
+        {
+            await transaction.CommitAsync();
+        }
+
+        public async Task RollBack(Microsoft.ServiceFabric.Data.ITransaction transaction)
+        {
+            transaction.Abort();
+        }
+
 
         /// <summary>
         /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
@@ -28,7 +176,8 @@ namespace BookStoreService
         /// <returns>A collection of listeners.</returns>
         protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
         {
-            return new ServiceReplicaListener[0];
+            //return new ServiceReplicaListener[0];
+            return this.CreateServiceRemotingReplicaListeners();
         }
 
         /// <summary>
